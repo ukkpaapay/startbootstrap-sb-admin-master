5d3f4aa661907659611b9d6b6e7462434d016bfe
ukkpaapay <38246106+ukkpaapay@users.noreply.github.com>
2019-05-12 18:52:58 +0700

    นับ count

--- js/count.js
+++ js/count.js
@@ -2,12 +2,36 @@
 
 $(document).ready(function () {
     console.log('Get data');
-    $.getJSON("json/customers.json", function (data, status) {
+    $.getJSON("json/customers.json", function (data1) {
+        var count=0
+        while (data1[count]) {
+            count++;
+        }
+        $('#data1').append(count);
+
+    });
+    $.getJSON("json/orders.json", function (data) {
         var count=0
         while (data[count]) {
             count++;
         }
-        $('#data').append(count);
+        $('#data2').append(count);
+
+    });
+    $.getJSON("json/products.json", function (data) {
+        var count=0
+        while (data[count]) {
+            count++;
+        }
+        $('#data3').append(count);
+
+    });
+    $.getJSON("json/suppliers.json", function (data) {
+        var count=0
+        while (data[count]) {
+            count++;
+        }
+        $('#data4').append(count);
 
     });
 });
